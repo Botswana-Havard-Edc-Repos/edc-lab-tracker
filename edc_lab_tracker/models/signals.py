7538a57a21f2756d14fc0462db6f5f24de41c91b
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-09-21 14:47:44 +0200

    Update signals.py

--- edc_lab_tracker/models/signals.py
+++ edc_lab_tracker/models/signals.py
@@ -4,8 +4,7 @@ from django.dispatch import receiver
 
 @receiver(post_save, weak=False, dispatch_uid="tracker_on_post_save")
 def tracker_on_post_save(sender, instance, raw, **kwargs):
-    """
-    Updates the lab_tracker history on post-save for 
+    """Updates the lab_tracker history on post-save for 
     model instances whose class is registered in site_lab_tracker.
     """
     if not raw:
@@ -18,8 +17,7 @@ def tracker_on_post_save(sender, instance, raw, **kwargs):
 
 @receiver(post_delete, weak=False, dispatch_uid="tracker_on_post_delete")
 def tracker_on_post_delete(sender, instance, **kwargs):
-    """
-    Deletes from the lab_tracker history for a model instance on post-save for model 
+    """Deletes from the lab_tracker history for a model instance on post-save for model 
     instances whose class is registered in site_lab_tracker.
     """
     # from edc.subject.lab_tracker.classes import site_lab_tracker
