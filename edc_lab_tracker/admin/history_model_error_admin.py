2347343735a5a15cfb6fc0b4f68f3dc9f36d3503
Keba462 <79043038+Keba462@users.noreply.github.com>
2023-09-21 15:09:02 +0200

    Update history_model_error_admin.py

--- edc_lab_tracker/admin/history_model_error_admin.py
+++ edc_lab_tracker/admin/history_model_error_admin.py
@@ -6,8 +6,8 @@ from ..models import HistoryModelError
 
 
 class HistoryModelErrorAdmin(BaseModelAdmin):
-    list_display = ('subject_identifier', 'group_name', 'test_code', 'value', 
-                    'value_datetime', 'source_model_name', 'source_app_label', 
+    list_display = ('subject_identifier', 'group_name', 'test_code', 'value',
+                    'value_datetime', 'source_model_name', 'source_app_label',
                     'source_identifier', 'history_datetime', 'modified')
     search_fields = ('subject_identifier', 'value', 'error_message')
     list_filter = ('group_name', 'source_model_name', 'source_app_label', 'test_code', 'modified')
